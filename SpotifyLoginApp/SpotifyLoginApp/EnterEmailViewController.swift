a7e9c130b6cbb68d4cbec2b8ce0eda008b7ba4d1
usa4060 <cocoa9805@naver.com>
2022-03-03 19:03:51 +0900

    Spotify Login App

--- SpotifyLoginApp/SpotifyLoginApp/EnterEmailViewController.swift
+++ SpotifyLoginApp/SpotifyLoginApp/EnterEmailViewController.swift
@@ -1,4 +1,6 @@
 import UIKit
+import Firebase
+
 
 class EnterEmailViewController : UIViewController{
     
@@ -28,6 +30,51 @@ class EnterEmailViewController : UIViewController{
     
     @IBAction func nextButtonTapped(_ sender: UIButton) {
         // FireBase에 이메일 및 비밀번호 값 입력하기
+        let email = emailTextField.text ?? ""
+        let password = passwordTextField.text ?? ""
+        
+        // 신규 사용자 생성
+        Auth.auth().createUser(withEmail: email, password: password) {
+            [weak self]  authResult, error in
+            guard let self = self else {return}
+            
+            // 이메일 회원가입 시 에러처리 구문
+            if let error = error {
+                let code = (error as NSError).code
+                switch code {
+                case 17007: // 이미 가입 한 계정일 때
+                    // 로그인하기
+                    self.loginUser(withEmail: email, password: password)
+                default:
+                    self.errorTextField.text = error.localizedDescription
+                }
+            }
+            else{
+                self.showMainViewController()
+            }
+            
+        }
+    }
+    
+    private func showMainViewController(){
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        let mainViewController = storyboard.instantiateViewController(identifier: "MainViewController" )
+        mainViewController.modalPresentationStyle = .fullScreen
+        navigationController?.show(mainViewController, sender: nil)
+    }
+    
+    private func loginUser(withEmail email : String, password : String){
+        Auth.auth().signIn(withEmail: email, password: password){[weak self] _, error in
+            guard let self = self else {return}
+            
+            if let error = error {
+                self.errorTextField.text = error.localizedDescription
+            }
+            else {
+                self.showMainViewController()
+            }
+            
+        }
     }
 }
 
--- SpotifyLoginApp/SpotifyLoginApp/MainViewController.swift
+++ SpotifyLoginApp/SpotifyLoginApp/MainViewController.swift
@@ -1,8 +1,30 @@
-//
-//  MainViewController.swift
-//  SpotifyLoginApp
-//
-//  Created by admin on 2022/03/02.
-//
+import UIKit
+import FirebaseAuth
 
-import Foundation
+
+
+class MainViewController : UIViewController{
+    
+    @IBOutlet weak var welcomeLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.interactivePopGestureRecognizer?.isEnabled = false
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.navigationBar.isHidden = true
+        
+        // 로그인이 되었다면, 현개 User의 Email을 가져와야 함
+        let email = Auth.auth().currentUser?.email ?? "고객"
+        
+        welcomeLabel.text = """
+        환영합니다!
+        \(email)님
+        """
+    }
+    
+    @IBAction func logoutButtonTapped(_ sender: UIButton) {
+        self.navigationController?.popToRootViewController(animated: true)
+    }
+}
